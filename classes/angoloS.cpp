56e4f8a89c8271251f29c8fbeca5b09ae5e1c35b
Andrea Grimaldi <grima@Epr-Pointer>
2014-03-13 12:41:07 +0100

    Add class for sexagesimal angles
    We must commend and document all

---
+++ classes/angoloS.cpp
@@ -0,0 +1,104 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2014  <copyright holder> <email>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "angoloS.h"
+#include <cmath>
+#include <iostream>
+angoloS::angoloS(int Xgradi, int Xprimi, double Xsecondi)
+{
+  secondi=Xsecondi+60*( Xprimi + 60 * Xgradi );
+  
+}
+angoloS::~angoloS()
+{
+
+}
+
+
+//resittuisce il valore in radianti
+
+double angoloS::GetRad()
+{
+return secondi/648000*M_PI;
+}
+
+
+
+
+//Operazioni algebriche generali 
+angoloS& angoloS::operator*( double sx)
+{
+  double secondiTmp=secondi*sx;
+  return *( new  angoloS(0,0,secondiTmp) );
+  
+}
+
+angoloS& angoloS::operator*( int sx)
+{
+  double secondiTmp=secondi*sx;
+  return *( new angoloS(0,0,secondiTmp) );
+  
+}
+
+
+angoloS& angoloS::operator/( int sx )
+{
+ ;
+  return *( new angoloS(0,0, secondi/sx) );
+  
+}
+
+angoloS& angoloS::operator/( double sx )
+{
+  int secondiTmp=secondi/sx;
+  return *( new angoloS(0,0,secondiTmp) );
+  
+}
+
+
+angoloS& angoloS::operator+(angoloS& sx)
+{
+ int secondiTmp=secondi+sx.secondi;
+  return *(new angoloS(0,0,secondiTmp));
+}
+
+
+std::ostream& operator<<(std::ostream& os, angoloS& other)
+{
+  
+  int secondiTmp = other.secondi;
+    os << secondiTmp/3600 << "°" << ( secondiTmp/60 )%60 << '\''<< secondiTmp%60 + (other.secondi - secondiTmp ) << "\'\'";
+    return os;
+}
+
+
+
+
+
+
+
+angoloS::angoloS(const angoloS& other)
+{
+
+}
+
+
+
+
+
---
+++ classes/angoloS.h
@@ -0,0 +1,59 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2014  <copyright holder> <email>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef ANGOLOS_H
+#define ANGOLOS_H
+
+#include <iostream>
+class angoloS
+{
+  
+friend
+  std::ostream& operator<<( std::ostream& os, angoloS& other);
+
+public:
+  angoloS(int Xgradi, int Xprimi = 0, double Xsecondi = 0.);
+  virtual ~angoloS();
+  
+
+  //ritorna il converte l'angolo i radianti
+  double GetRad();  
+  
+  //Operatori generici
+  angoloS& operator*( double sx);
+  angoloS& operator/( double sx);
+  angoloS& operator*( int sx);
+  angoloS& operator/( int sx );
+  angoloS& operator+( angoloS& sx);
+  angoloS& operator=(const angoloS& other){ this->secondi=other.secondi; return *this;};
+
+
+private:
+  
+
+  double secondi;
+  
+  //copie private
+  angoloS(const angoloS& other);
+};
+
+  
+
+
+#endif // ANGOLOS_H
